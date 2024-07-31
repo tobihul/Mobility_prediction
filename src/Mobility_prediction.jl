282a48a9d3be831d06ad7ba3f0483ba82861c324
Tobias Hulleman <uqthulle@uq.edu.au>
2024-07-31 11:08:06 +1000

    Publication of version 0.1.0

--- src/Mobility_prediction.jl
+++ src/Mobility_prediction.jl
@@ -4,11 +4,16 @@ module Mobility_prediction
 using CSV, Statistics, DataFrames, PubChemCrawler, StatsPlots
 using LinearAlgebra, ScikitLearn, Random, MLJ, PyCall, Conda
 
+function __init__()
+    skl = pyimport("sklearn.ensemble")
+    jl = pyimport("joblib")
+    pd = pyimport("padelpy")
+    global skl, jl, pd
+end
+
 include("All functions.jl")
 
 
-export interpolate_B_modifier, calculate_leverage, remove_missing_and_outliers, TPR_FDR, 
-train_test_split_no_leakage_classifier, assign_labels, strat_labels, remove_outliers_IQR, remove_missing_identifiers,
-append_occurrences_folds, smiles_to_mobility
+export smiles_to_mobility
 
 end # module Mobility_prediction
