6dd58c458c70862e7deceae3aed69c830db38d60
Tobias Hulleman <uqthulle@uq.edu.au>
2024-07-31 11:10:21 +1000

    Publication of version 0.1.0

--- src/Mobility_prediction.jl
+++ src/Mobility_prediction.jl
@@ -4,6 +4,10 @@ module Mobility_prediction
 using CSV, Statistics, DataFrames, PubChemCrawler, StatsPlots
 using LinearAlgebra, ScikitLearn, Random, MLJ, PyCall, Conda
 
+const skl = Ref{PyObject}()
+const jl = Ref{PyObject}()
+const pd = Ref{PyObject}()
+
 function __init__()
     skl = pyimport("sklearn.ensemble")
     jl = pyimport("joblib")
